c6a16acf41183394b8b635a97e898fc6a886d4f8
Sameer <69124381+SAMEER-40@users.noreply.github.com>
2024-07-19 18:29:21 +0530

    optimized

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
-import { preloadImages, preloadFonts } from './utils';
+// Importing utility function for preloading images
+import { preloadImages, preloadImages2, preloadFonts } from './utils';
 import { Row } from './row';
 import { gsap } from 'gsap';
 import { Flip } from 'gsap/Flip';
+// Importing StackMotionEffect classes from different effect files with renamed imports to avoid name conflicts
+import { StackMotionEffect as StackMotionEffect1 } from './effect-1/stackMotionEffect.js';
+import { StackMotionEffect as StackMotionEffect2 } from './effect-2/stackMotionEffect.js';
+import { StackMotionEffect as StackMotionEffect3 } from './effect-3/stackMotionEffect.js';
+
 gsap.registerPlugin(Flip);
 
 // preview Items
@@ -243,4 +249,52 @@ closeCtrl.addEventListener('click', () => {
 // Preload images and fonts
 Promise.all([preloadImages('.cell__img-inner'), preloadFonts('gdf6msi')]).then(() => {
     document.body.classList.remove('loading')
+});
+
+
+// Registers ScrollTrigger plugin with GSAP for scroll-based animations.
+gsap.registerPlugin(ScrollTrigger);
+
+// Initialize function to set up motion effects and animations
+const init = () => {
+  // Apply the first stack motion effect to all elements with a specific data attribute
+  document.querySelectorAll('[data-stack-1]').forEach((stackEl) => {
+    new StackMotionEffect1(stackEl);
+  });
+  // Apply the second stack motion effect to all elements with a different specific data attribute
+  document.querySelectorAll('[data-stack-2]').forEach((stackEl) => {
+    new StackMotionEffect2(stackEl);
+  });
+  // Apply the third stack motion effect to all elements with yet another specific data attribute
+  document.querySelectorAll('[data-stack-3]').forEach((stackEl) => {
+    new StackMotionEffect3(stackEl);
+  });
+
+  // Select all grid intro card elements and apply animations on scroll
+  const introCards = document.querySelectorAll('.intro1 .card');
+  introCards.forEach(introCard => {
+    gsap.to(introCard, {
+      ease: 'power1.in',
+      startAt: {
+        transformOrigin: '100% 50%',
+        filter: 'brightness(70%)'
+      },
+      rotationX: () => -60,
+      yPercent: () => gsap.utils.random(-100,0),
+      z: () => gsap.utils.random(-100,0),
+      filter: 'brightness(0%)',
+      scrollTrigger: {
+        trigger: introCard,
+        start: 'clamp(top bottom)',
+        end: 'clamp(bottom top)',
+        scrub: true,
+      }
+    });
+  });
+};
+
+// Preloading images and initializing setup when complete
+preloadImages2('.grid__img').then(() => {
+  document.body.classList.remove('loading');
+  init();
 });
